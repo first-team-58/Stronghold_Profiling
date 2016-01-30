171d23a4482bad71142141d5dabddc7a00c07c14
Teacher <teacher@Teachers-MacBook-Air-4.local>
2016-01-30 18:26:25 -0500

    scanfordata working, cleandata needs id initializing fixed

--- app.js
+++ app.js
@@ -3,20 +3,34 @@ var db = new PouchDB('scouting');
 function save(formData) {
     db.put(formData);
 }
-        
-function cleanData(dataset) {
-    $(document).ready(function(){
-        $("input").each(function){
-            dataset += $(this).name()+:+$(this).value()+,;  
-        }
-        $("select").each(function){
-            dataset += $(this).name()+:+$(this).value()+,;  
-        }
-    })
+
+function appendData(dataset, name, value) {
+    console.log(name + ", " + value + ", " + dataset);
+    dataset[name] = value;
+}
+
+function scanForData(elementType, dataset) {
+    $(elementType).each(function () {
+        var name = $(this).attr("name");
+        var value = $(this).val();
+        appendData(dataset, name, value);
+    });
+}
+
+function cleanData() {
+    var dataset = {_id:"001"};
+    
+    var types = ["input", "select", "textarea"];
+    
+    for(i=0; i<types.length; i++){
+        scanForData(types[i], dataset);
+    }
+    
     return dataset;
 }
 
 function saveMatchForm() {
     var formData = cleanData();
+    console.log(formData);
     save(formData);
 }
